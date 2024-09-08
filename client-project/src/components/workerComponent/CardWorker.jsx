6420921ef693139eb4b830e000fea0d52551100b
Adityasputra <aditsputra.cv@gmail.com>
2024-09-08 15:04:16 +0700

    Fix: FETCH UPDATE PROFILE WORKER

--- client-project/src/components/workerComponent/CardWorker.jsx
+++ client-project/src/components/workerComponent/CardWorker.jsx
@@ -3,7 +3,7 @@
 // FFFFFF card
 // FAF9FE bg
 
-export default function CardWorker() {
+export default function CardWorker({ work }) {
   return (
     <div className="bg-[#FFFFFF] p-6 rounded-lg shadow">
       <div className="flex items-center space-x-4">
@@ -13,10 +13,7 @@ export default function CardWorker() {
         <span className="bg-[#FAF9FE] p-1 rounded text-xs">Household</span>
       </div>
       <div>
-        <p className="mt-4 text-sm text-[#1D204C]">
-          Tolong, bersihkan kamar saya, jika bisa tolong cari semua kecoa dan
-          tikusnya dan makan
-        </p>
+        <p className="mt-4 text-sm text-[#1D204C]">{work.description}</p>
       </div>
       <hr className="mt-4" />
       <div className="mt-4 flex justify-between items-center">
@@ -24,7 +21,7 @@ export default function CardWorker() {
           <div className="w-12 h-12 bg-[#FAF9FE] rounded-full" />
           <h3 className="text-lg font-semibold">Aditya</h3>
         </div>
-        <span className="text-[#1D204C]">$80/h</span>
+        <span className="text-[#1D204C]">${work.fee}/h</span>
       </div>
     </div>
   );
