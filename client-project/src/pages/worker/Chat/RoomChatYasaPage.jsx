fd35caf29e1221fe14d84944da114248b6a67e75
Adityasputra <aditsputra.cv@gmail.com>
2024-09-11 16:26:08 +0700

    Fix: fix name chat Page Yasa and Jalu

--- client-project/src/pages/worker/Chat/RoomChatYasaPage.jsx
+++ client-project/src/pages/worker/Chat/RoomChatYasaPage.jsx
@@ -1,48 +1,49 @@
-import { useEffect, useRef, useState } from "react";
-import { io } from "socket.io-client";
-import { useLocation, useParams } from "react-router-dom";
-import ChatEnd from "../../../components/ChatEnd";
-import ChatStart from "../../../components/ChatStart";
-import ProfileChat from "../../../components/workerComponent/ProfileChat";
-import StarRating from "../../../components/workerComponent/StarRating";
+import { useEffect, useRef, useState } from 'react';
+import ChatEnd from '../../../components/ChatEnd';
+import ChatStart from '../../../components/ChatStart';
+import ProfileChat from '../../../components/workerComponent/ProfileChat';
+import { io } from 'socket.io-client';
+import { useLocation, useParams } from 'react-router-dom';
 
-export default function RoamChatClientPage() {
+export default function RoomChatYasaPage() {
   const [chats, setChats] = useState([]);
-  const [message, setMessage] = useState("");
+
+
+  const [message, setMessage] = useState('');
   const [socket, setSocket] = useState(null);
   const [senderId, setSenderId] = useState(null);
-  const [chatId, setChatId] = useState("");
+  const [chatId, setChatId] = useState('');
   const chatContainerRef = useRef(null);
 
   const { id } = useParams();
   const location = useLocation();
   const order = location.state?.order || {};
+  console.log(order, "ini order")
 
   useEffect(() => {
-    const storedRole = localStorage.getItem("role");
-    const newSocket = io("http://localhost:3000");
+    const storedRole = localStorage.getItem('role');
+    const newSocket = io('http://localhost:3000');
 
     // Masuk ke room
     setSocket(newSocket);
-    newSocket.emit("join_room", id);
+    newSocket.emit('join_room', id);
 
     // Gabungin data chat
-    newSocket.on("receive_message", (data) => {
+    newSocket.on('receive_message', (data) => {
       setChats((curr) => [...curr, data]);
     });
 
-    // Set sender id
-    newSocket.on("joined_room", (data) => {
-      console.log(data, "<<<<< ini join room");
+    // set sender id
+    newSocket.on('joined_room', (data) => {
+      console.log(data, '<<<<< ini join room');
       console.log(data.chats);
       setChats(data.chats.contents);
 
-      if (storedRole === "jalu") {
+      if (storedRole === 'jalu') {
         setSenderId(data.currJob.clientId);
-        console.log(data.currJob.clientId, "<<< ini client id");
-      } else if (storedRole === "yasa") {
+        // console.log(data.currJob.clientId, "<<< ini client id");
+      } else if (storedRole === 'yasa') {
         setSenderId(data.currJob.workerId);
-        console.log(data.currJob.workerId, "<<< ini worker id");
       }
 
       setChatId(data.currJob.chatId);
@@ -62,49 +63,47 @@ export default function RoamChatClientPage() {
   }, [chats]);
 
   const sendMessage = () => {
-    if (message.trim() !== "" && socket) {
+    if (message.trim() !== '' && socket) {
       const messageObj = {
         room: id,
+        senderId: senderId,
         message: message,
         createdAt: new Date(),
-        senderId: senderId,
       };
-      socket.emit("send_message", messageObj, chatId, senderId);
-      console.log(messageObj, "Ini massage");
+      socket.emit('send_message', messageObj, chatId, senderId);
+
       // setChats((curr) => [...curr, messageObj]);
-      setMessage("");
+      setMessage('');
+      console.log(chats, 'currrr');
     }
   };
-
   return (
     <>
       <div className="flex bg-[#FAF9FE] gap-4 h-[75vh] overflow-hidden p-6 rounded-xl">
-        {/* {JSON.stringify(chats)}; */}
-        <div className="w-72 bg-[#FFFFFF]  p-4 rounded-2xl">
+      <div className="w-72 bg-[#FFFFFF]  p-4 rounded-2xl">
           <div className="mb-6">
             <div className="flex items-center justify-center flex-col">
               <div className="stat-figure text-secondary">
                 <div className="avatar online">
                   <div className="w-16 rounded-full">
-                    <img src={order.workers[0].profilePicture} />
+                    <img src={order.client.profilePicture} />
                   </div>
                 </div>
               </div>
 
               <div className="text-center">
                 <p className="font-semibold text-[#1D204C] text-lg">
-                  {order.workers[0].name}
+                  {order.client.name}
                 </p>
                 <p className="text-xs text-[#05ECAE] mt-1">Online</p>
               </div>
             </div>
           </div>
         </div>
+
         <div className="flex-1 p-6 flex flex-col bg-[#FAF9FE] rounded-2xl shadow-lg">
           <div className="flex justify-between items-center mb-4">
-            <h1 className="text-2xl font-semibold text-[#1D204C]">
-              {order.workers[0].name}
-            </h1>
+            <h1 className="text-2xl font-semibold text-[#1D204C]">{order.client?.name}</h1>
           </div>
 
           <hr className="border-t border-[#FAF9FE] mb-4" />
@@ -115,8 +114,7 @@ export default function RoamChatClientPage() {
           >
             {chats.map((chat, index) =>
               chat.senderId === senderId ? (
-                (console.log(chat, "ini chat"),
-                (<ChatEnd key={index} message={chat.message} />))
+                <ChatEnd key={index} message={chat.message} />
               ) : (
                 <ChatStart key={index} message={chat.message} />
               )
@@ -130,7 +128,7 @@ export default function RoamChatClientPage() {
               placeholder="Write your message..."
               value={message}
               onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+              onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
             />
             <button
               className="ml-2 bg-[#1D204C] text-[#FAF9FE] px-3 py-2 rounded-lg shadow-md hover:bg-[#2a2b38] transition"
